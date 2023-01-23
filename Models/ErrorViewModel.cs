3315b00b1b6d2567eea43d4d26d72fdb703fd701
Hunter <46310167+hunterthis@users.noreply.github.com>
2023-01-22 18:20:19 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReversePomodoro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
